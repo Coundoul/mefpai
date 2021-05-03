6339fad485370b61db6be4a4bf371c0cede82645
Ablaye95 <coundoulabdoulaye471@gmail.com>
2021-05-03 13:37:05 +0000

    update-

--- src/main/java/sn/coundoul/gestionpatrimoine/security/AuthoritiesConstants.java
+++ src/main/java/sn/coundoul/gestionpatrimoine/security/AuthoritiesConstants.java
@@ -9,6 +9,26 @@ public final class AuthoritiesConstants {
 
     public static final String USER = "ROLE_USER";
 
+    public static final String ROLE_CP_ = "ROLE_CP";
+
+    public static final String ROLE_CS = "ROLE_CS";
+
+    public static final String ROLE_CHP = "ROLE_CHP";
+
+    public static final String ROLE_CHM = "ROLE_CHM";
+
+    public static final String ROLE_IN = "ROLE_IN";
+
+    public static final String ROLE_MF = "ROLE_MF";
+
+    public static final String ROLE_TC = "ROLE_TC";
+
+    public static final String ROLE_DT = "ROLE_DT";
+
+    public static final String ROLE_OR = "ROLE_OR";
+
+    public static final String ROLE_DIR = "ROLE_DIR";
+
     public static final String ANONYMOUS = "ROLE_ANONYMOUS";
 
     private AuthoritiesConstants() {}
